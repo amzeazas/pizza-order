ab05c302244b9126eec1d3ba63f9053f74713594
Alexa <amzeazas@comcast.net>
2015-09-18 08:05:17 -0700

    Add spec for adding new pizza object.

---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe('Pizza', function() {
+  it("creates a new pizza with the given specifications", function() {
+    var testPizza = new Pizza("1", "large");
+    expect(testPizza.quantity).to.equal("1");
+    expect(testPizza.size).to.equal("large");
+    expect(testPizza.toppings).to.equal([]);
+  });
+});
